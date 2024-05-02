b46e5602227ebc23a8d9c791093339534e445792
Sushil <sushillovanshi8@gamil.com>
2024-05-02 18:19:41 +0530

    Python add on started new file

---
+++ first.py
@@ -0,0 +1,26 @@
+print("hello world!")
+# sect over flow 
+a = 10
+b = 20
+Rouge_Sushil =30
+age = 21 
+print(a)
+print(b)
+print(Rouge_Sushil)
+print (age)
+
+# miltipal value one line print
+x=y=z=10
+print(x)
+print(y)
+print(z)
+
+# multipal vlaue singale line 
+x,y,z=1,2,3
+print(x)
+print(y)
+print(z)
+
+
+#mote on sinlge line comment (#)        miltiy line comment ('''    ''')
+
