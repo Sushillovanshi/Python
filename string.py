c02c65752deb29841d36baafed18959d6e9ecd48
Sushil <sushillovanshi8@gamil.com>
2024-05-08 18:32:59 +0530

    string new file

--- string.py
+++ string.py
@@ -2,18 +2,18 @@
 # Dublicate charactor \ object are allowed  (ex: >> str:'sushil')
 # Sequential datA.type "sushil" (i.e order is fixed)
 # indexing and slicing also allowed
-
-str="sushil"
-print(str.index('s'))
-# o\p=0
-str="hellorougehacker"
-print(type(str))
-# o\p= str
-str='rogue_hack'
-print(str)
-# o\p rogue_hack
-str1=''' hello 'gyus rogue'"hack" by ka'''
-print(str1)
+''''''
+# str="sushil"
+# print(str.index('s'))
+# # o\p=0
+# str="hellorougehacker"
+# print(type(str))
+# # o\p= str
+# str='rogue_hack'
+# print(str)
+# # o\p rogue_hack
+# str1=''' hello 'gyus rogue'"hack" by ka'''
+# print(str1)
 # str use by ''' code and under by '' <==> "" using a string 
 
 
@@ -23,11 +23,94 @@ print(str1)
 * mathematic operation 
 
 '''
-str='sushil'
+'''str='sushil'
 str1='lovanshi'
 print(str+str1)
 print(max(str))
 print(min(str))
 print(len(str))
-print(chr(str))
+print(chr(str))'''
+
+# list
+
+# my_list=[10,20,30,40,50,60]
+# x =my_list.index(20)
+# my_list[x]=50
+# print(my_list)
+# print(max(my_list))
+
+# my_list=[10,20,30,40,50,60]
+#           append 
+# my_list.append('60')
+# print(my_list)
+
+#   clear
+
+# my_list.clear()
+# print(my_list)
+
+#   list
+# x=[]
+# print(type(x))
+
+#   copy
+ 
+# x=[10,20,30,40,50,60]
+# y=x
+# print(y)     
+# x.remove(10)
+# print(y)
+
+#       count
+# my_list=[10,20,30,40,50,60]
+# my_list.count(10)
+# print(my_list.count(10))
+
+#      extands  value extands
+
+# my_list1=[70,80]
+# my_list=[10,20,30,40,50,60]
+# my_list.extend(my_list1)
+# print(my_list)
+# my_list1.extend(my_list)
+# print(my_list1)
+
+#  insert one value add 
+
+# my_list=[10,20,30,40,50,60]
+# my_list.insert(3,100)
+# print(my_list)
+
+#  pop  last object delet
+
+# my_list=[10,20,30,40,50,60]
+# my_list.pop()
+# print(my_list)
+
+# remove value
+
+# my_list=[10,20,30,40,50,60]
+# my_list.remove(100)
+# print(my_list)
+
+# reverse value reverse 
+
+# my_list=[10,20,30,40,50,60]
+# my_list.reverse()
+# print(my_list)
+
+# sort arranging order asecding order
+
+my_list=[10,20,30,40,50,60]
+my_list.sort()
+print(my_list)
+
+#  arranging order decinding order
+
+
+my_list=[10,20,30,40,50,60]
+my_list.sort( reverse=True)
+print(my_list)
+
+
 
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,3 @@
-hack
+my_list=[10,20,30,40,50,60]
+# my_list.reverse()
+# print(my_list)
